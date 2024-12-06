e94d184d46702be554aaa547f986a7ef56703bae
Maleesha Dilshan <maleeshahirug@gmail.com>
2024-12-07 01:04:49 +0530

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,44 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import LoginForm from './pages/LoginForm';
+import RegisterForm from './pages/RegisterForm';
+import { AuthProvider } from './contexts/AuthContext';
+import ProfilePage from './pages/ProfilePage';
+import UserDashboard from './pages/UserDashboard';
+import VotingPage from './pages/VotingPage';
+import AdminDashboard from './pages/AdminDashboard';
+import ElectionCreationPage from './pages/ElectionCreationPage';
+import { AdminReportPage } from './pages/AdminReportPage';
+import UserReportPage from './pages/UserReportPage';
+import SecurityDashboard from './pages/SecuirtyDashboard';
+import PublicDashboard from './pages/PublicDashboard';
+
 function App() {
-  return <h1> Hello world </h1>;
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PublicDashboard />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/user" element={<UserDashboard />} />
+          <Route path="/vote" element={<VotingPage />} />
+          <Route path="/create-election" element={<ElectionCreationPage />} />
+          <Route path="/admin-report" element={<AdminReportPage />} />
+          <Route path="/user-report" element={<UserReportPage />} />
+          <Route path="/secuirity" element={<SecurityDashboard />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
